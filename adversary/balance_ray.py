d90206ac7e10ee1e1a77e67ce82ac5b64c84d80c
XiaoSanGit <849304059@qq.com>
2020-10-20 21:06:05 +0800

    change it to rh's. existing bug: may not exist the other label for AE of the same img

--- adversary/balance_ray.py
+++ adversary/balance_ray.py
@@ -1,7 +1,6 @@
 import argparse
 import copy
 from typing import Union, Tuple
-
 import numpy as np
 import torch
 from overloading import overload
@@ -21,14 +20,15 @@ class BalanceAdversary(Adversary):
         if kwargs['test']:
             self.sampler.dataset = Subset(self.sampler.dataset, [i for i in range(2000)])
         self.penalty_standard = []
+        self.sub_penalty = np.zeros((self.sampler.num_classes, self.sampler.num_classes))
         self.penalty_matrix = np.zeros((self.sampler.num_classes, self.sampler.num_classes))
-        self.temp_penalty = np.zeros((self.sampler.num_classes, self.sampler.num_classes))
+        #self.temp_penalty = np.zeros((self.sampler.num_classes, self.sampler.num_classes))
         self.statistic = np.zeros((self.sampler.num_classes, self.sampler.num_classes))
         self.max_iter = 200
         self.overshoot = .02
         self.max_select_per_direction = 50
 
-    def get_perturbation(self, item: Union[int, torch.Tensor]) -> Tuple[float, Tuple[int, int]]:
+    def get_perturbation(self, item: Union[int, torch.Tensor], dis:Tuple = [], sec: int  = 0) -> (Tuple[float, Tuple[int, int]], torch.Tensor):
         """
 
         :param item: refers to current indices
@@ -56,9 +56,12 @@ class BalanceAdversary(Adversary):
         penalty[i] = np.inf
         output[0, i].backward(retain_graph=True)
         origin_grad = x.grad.clone().detach()
+
         for j in range(self.sampler.num_classes):
             # i -> j
             if j == i:
+                continue
+            if sec == 1 and dis[1] == j:
                 continue
             r_tot = torch.zeros_like(sample, device=self.device)
             while pert_label == i:
@@ -83,13 +86,12 @@ class BalanceAdversary(Adversary):
             pert_label = i
         index = penalty.argsort()[0]
 
-        return penalty[index], (i, index)
+        return penalty[index], (i, index), r_tot
 
-    def punish(self, i: int, j: int):
-        k = int(len(self.penalty_standard) * self.statistic[i, j] / self.max_select_per_direction) - 1
-        self.temp_penalty[i, j] = self.penalty_standard[k] - self.penalty_standard[0] - self.penalty_matrix[i, j]
-        self.penalty_matrix[i][j] = self.penalty_standard[k] - self.penalty_standard[0]
-        self.statistic[i][j] += 1
+    def punish(self, i: int, j: int, pert):
+          if self.penalty_matrix[i][j] == 0:
+             self.penalty_matrix[i][j] = pert
+             self.statistic[i][j] += 1
 
 
     def choose(self, method: str, budget: int):
@@ -99,33 +101,52 @@ class BalanceAdversary(Adversary):
             selecting_pool = set(list(range(len(self.sampler.dataset))))
             selecting_pool.difference_update(self.sampler.indices)
             selecting_pool = list(selecting_pool)
-            self.temp_penalty = np.zeros([self.sampler.num_classes, self.sampler.num_classes])
+            #self.temp_penalty = np.zeros([self.sampler.num_classes, self.sampler.num_classes])
             penalty_list = []
+            penalty_list1 = []
             direction_list = []
+            direction_list1 = []
+            pert_list = []
+            pert_list1 = []
             for i in tqdm(selecting_pool):
-                penalty, direction = self.get_perturbation(i)
+                penalty, direction, pert = self.get_perturbation(i)
                 penalty_list.append(penalty)
                 direction_list.append(direction)
+                pert_list.append(pert)
+
+            for i in tqdm(selecting_pool):
+                penalty, direction, pert = self.get_perturbation(i, direction_list[i], 1)
+                penalty_list1.append(penalty)
+                direction_list1.append(direction)
+                pert_list1.append(pert)
+
             self.penalty_standard = penalty_list[:]
-            arg_penalty = np.argsort(penalty_list)
+            self.sub_penalty = list(np.abs(np.array(pert_list)- np.array(pert_list1)))
+            arg_penalty = np.argsort(self.penalty_standard)
+            add_penalty = np.sort(self.sub_penalty)
+            add_penalty_indice = np.argsort(self.sub_penalty)
             punished = set()  # For temp punishment
             chosen = set()
             current = 0
             while len(chosen) < budget:
                 current_index = arg_penalty[current]
-                i, j = direction_list[current_index]
-                if self.temp_penalty[i, j] == 0:
-                    chosen.add(current_index)
-                    self.punish(i, j)
-                    current += 1
-                elif current_index in punished:
-                    chosen.add(current_index)
-                    self.punish(i, j)
-                    current += 1
-                else:
-                    penalty_list[current_index] += self.temp_penalty[i, j]
-                    arg_penalty = np.argsort(penalty_list)
-                    punished.add(current_index)
+                i, j = direction_list[add_penalty_indice[current_index]]
+                pert = add_penalty[current_index]
+                chosen.add(current_index)
+                self.punish(i, j, pert)
+
+                # if self.temp_penalty[i, j] == 0:
+                #     chosen.add(current_index)
+                #     self.punish(i, j,pert)
+                #     current += 1
+                # elif current_index in punished:
+                #     chosen.add(current_index)
+                #     self.punish(i, j,pert)
+                #     current += 1
+                # else:
+                #     penalty_list[current_index] += self.temp_penalty[i, j]
+                #     arg_penalty = np.argsort(penalty_list)
+                #     punished.add(current_index)
             real_chosen = [selecting_pool[i] for i in chosen]
             labels = self.query_tensor(QueryWrapper(self.sampler.dataset, real_chosen))
             self.sampler.extend(real_chosen, labels)
